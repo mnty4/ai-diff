7835ec462b415b7b7752cc40b623ff84a324d339
lukas <zenikyn@gmail.com>
2025-10-09 12:28:43 +1100

    make header bold

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,7 +3,9 @@ import { titleFont } from "@/app/fonts";
 export default function Header() {
   return (
     <header>
-      <span className={`${titleFont.className} text-3xl`}>AI Edit...</span>
+      <span className={`${titleFont.className} text-3xl font-bold`}>
+        AI Edit...
+      </span>
     </header>
   );
 }
